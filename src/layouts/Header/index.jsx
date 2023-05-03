bc2b267bfce54385b9c5c2ab1a8121cae6b60d3a
farhan3311 <farhansidat3311@gmail.com>
2023-05-03 16:56:45 +0100

    video link added

--- src/layouts/Header/index.jsx
+++ src/layouts/Header/index.jsx
@@ -21,22 +21,28 @@ export default function Header() {
   }
   return (
     <>
-      <nav className="navbar navbar-expand-lg" style={{backgroundColor: `${theme.primBG}`}}>
+      <nav className="navbar navbar-expand-lg" style={{backgroundColor: `${theme.primBG}`, color: `${theme.primText}`}}>
         <div className="container">
-          <a className="navbar-brand" href="/">F  O C A L I S E</a>
+          <a className="navbar-brand" href="/" style={{color: `${theme.primText}`}}>F  O C A L I S E</a>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">Home</a>
+                <a className="nav-link active" aria-current="page" href="/" style={{color: `${theme.primText}`}}>Home</a>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/sensory">Sensory</a>
+                <a className="nav-link" href="/sensory" style={{color: `${theme.primText}`}}>Sensory</a>
               </li>
-              <li className="nav-item dropdown">
-                <a className="nav-link" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" onClick={handleTheme}>
+             
+              <li className="nav-item">
+                <a className="nav-link" href="/profile" style={{color: `${theme.primText}`}}>Profile</a>
+              </li>
+            </ul>
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0 d-flex flex-row-reverse">
+            <li className="nav-item dropdown">
+                <a className="nav-link" href="#" style={{color: `${theme.primText}`}} role="button" data-bs-toggle="dropdown" aria-expanded="false" onClick={handleTheme}>
                   Change Theme
                 </a>
                 {/* <a className="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" onClick={handleTheme}>
@@ -47,10 +53,8 @@ export default function Header() {
                   <li><a className="dropdown-item" href="#">Another action</a></li>
                 </ul> */}
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="/profile">Profile</a>
-              </li>
             </ul>
+            
           </div>
         </div>
       </nav>
