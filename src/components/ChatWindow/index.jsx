ec83198fb237274519fe004a752047c3cb4e2798
“Vitali-G” <vitali.goubski@gmail.com>
2023-05-05 12:22:15 +0100

    Added body double Chat functionality, but it currently thinks its a tiger...

--- src/components/ChatWindow/index.jsx
+++ src/components/ChatWindow/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import './ChatWindow.css'
 import { Configuration, OpenAIApi } from "openai";
 
@@ -10,15 +10,45 @@ const openai = new OpenAIApi(
 );
 
 export default function ChatWindow() {
-  function hello() {
-    console.log("hello");
+  const [chatArray, setChatArray] = useState([{"role": "system", "content": "You are an Tiger. Respond only sounds a Tiger would make. Use maximum 2 'words' per response"}])
+  const [chatText, setChatText] = useState("")
+  
+  async function chatBot() {
+    const res = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: chatArray,
+    });
+    const data = res.data.choices[0].message["content"];
+    console.log(data);
+    setChatArray(chatArray => [...chatArray, {"role": "assistant", "content": data}])
+    console.log(chatArray);
   }
 
   useEffect(() => {
-    hello()
+    console.log(chatArray);
   }, [])
+
+  function handleInput(e) {
+    const newInput = e.target.value;
+    setChatText(newInput)
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setChatArray(chatArray => [...chatArray, {"role": "user", "content": chatText}])
+    chatBot()
+    setChatText("")
+  }
+  
   return (
-    <div>index-Hello</div>
+    <>
+      <ul style={{listStyleType: "none"}}>
+        {chatArray.slice(1).map((line, i) => (<li key={i}>{line.content}</li>))}
+      </ul>
+      <form onSubmit={handleSubmit}>
+        <input className="chat-input" value={chatText} type="text" onChange={handleInput} placeholder="Chat with your Body Double"/>
+        <button className="chat-submit" type="submit">XXX</button>
+      </form>
+    </>
   )
 }
-
