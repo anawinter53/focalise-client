7348a91aa30b59cac3930476b2b8548ea09446b6
anawinter53 <anatolia.winter@yahoo.com>
2023-05-10 20:01:40 +0100

    make tasks progress dot render dynamically

--- src/components/Tasks/tasks.jsx
+++ src/components/Tasks/tasks.jsx
@@ -1,12 +1,13 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useTheme } from '../../contexts'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faCircle } from '@fortawesome/free-solid-svg-icons'
 import { AddTaskModal } from '../../components'
+import { BsCircle, BsRecordCircleFill, BsFillCheckCircleFill } from 'react-icons/bs';
 
 export default function TasksPage({tasks, setRender}) {
     const { theme } = useTheme()
     useEffect(() => { document.body.style.backgroundColor = `${theme.primColor}` }, )
+
+
 
   return (
     <div>
@@ -14,24 +15,61 @@ export default function TasksPage({tasks, setRender}) {
             <div className="d-flex aligns-items-center justify-content-center position-relative">
                 <div className="container text-center pt-3 shadow rounded position-absolute" style={{backgroundColor: `${theme.primBG}`, color: `${theme.primText}`,  top: '50%', left: '50%', transform: `translate(-50%,50%)`}}>
                     <button className="btn btn-success position-absolute top-0 start-0" onClick={() => setRender('')}>Back</button>
-                    <button open={showModal} className="btn btn-danger position-absolute top-0 end-0" onClick={AddTaskModal}>Add Task</button>
+                    <button className="btn btn-danger position-absolute top-0 end-0" onClick={AddTaskModal}>Add Task</button>
                     <div className="row justify-content-center p-5" style={{}}>  
                         {tasks ? tasks.map((task, i) => (
-                            <div key={i} className='d-flex justify-content-between'>
-                            {console.log(task.task_name)}
-                            <FontAwesomeIcon icon={faCircle} />
-                            <p name={task.task_name} >{task.task_name}</p>
-                            <p name={task.task_desc} >{task.task_desc}</p>
-                            <p>Url here</p>
-                            <p>Urgent</p>
-                            <p>by 23/4</p>
+                            <div key={i} className='row'>
+                                <div  className='col-2'>
+                                    {<CheckCircle task={task}/>}
+                                </div>
+                                <div className='col-2'>
+                                    <p name={task.task_name} >{task.task_name}</p>
+                                </div>
+                                <div className='col-2'>
+                                    <p name={task.task_desc} >{task.task_desc}</p>
+                                </div>
+                                <div className='col-2'>
+                                    <p>Url here</p>
+                                </div>
+                                <div className='col-2'>
+                                    <p>Urgent</p>
+                                </div>
+                                <div className='col-2'>
+                                    <p>by 23/4</p>
+                                </div>
                             </div>
-                        )) : undefined
-                        }   
+                        )) : undefined }   
                     </div>
                 </div>            
             </div>
             </section>
     </div>
   )
+}
+
+
+function CheckCircle({task}) {
+    const [activeComponent, setActiveComponent] = useState(undefined)
+    console.log(task)
+
+
+    useEffect(() => {
+        switch (task.task_status) {
+            case 'Not Started':
+                setActiveComponent(<BsCircle />)
+                break;
+            case 'Started':
+                setActiveComponent(<BsRecordCircleFill />)
+                break;
+            default:
+                setActiveComponent(<BsFillCheckCircleFill />)
+                break;
+        }
+    }, [task])
+
+    return (
+        <div>
+            {activeComponent}
+        </div>
+    )
 }
