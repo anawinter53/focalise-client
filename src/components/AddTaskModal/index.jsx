9dc0bbae5407f1c2ffce7f87ea43b548436c2e6c
anawinter53 <anatolia.winter@yahoo.com>
2023-05-10 20:20:31 +0100

    added a modal and close open that works omg

--- src/components/AddTaskModal/index.jsx
+++ src/components/AddTaskModal/index.jsx
@@ -1,12 +1,15 @@
 import { useTheme } from '../../contexts'
 
-export default function AddTaskModal({ open }) {
+export default function AddTaskModal({open, setModal}) {
     const { theme } = useTheme()
     if (!open) return null
+
+
 
     return (
         <div className='position-absolute text-center shadow p-3 rounded position-absolute' style={{backgroundColor: `${theme.primBG}`, color: `${theme.primText}`, top: '50%', left: '50%', transform: `translate(-50%,50%)`, zIndex: 999}}>
             <h1>Add Task</h1>
-    </div>
+            <button onClick={() => setModal(false)}>Close</button>
+        </div>
     )
 }
--- src/components/Tasks/tasks.jsx
+++ src/components/Tasks/tasks.jsx
@@ -5,6 +5,7 @@ import { BsCircle, BsRecordCircleFill, BsFillCheckCircleFill } from 'react-icons
 
 export default function TasksPage({tasks, setRender}) {
     const { theme } = useTheme()
+    const [modal, setModal] = useState(false)
     useEffect(() => { document.body.style.backgroundColor = `${theme.primColor}` }, )
 
 
@@ -15,7 +16,7 @@ export default function TasksPage({tasks, setRender}) {
             <div className="d-flex aligns-items-center justify-content-center position-relative">
                 <div className="container text-center pt-3 shadow rounded position-absolute" style={{backgroundColor: `${theme.primBG}`, color: `${theme.primText}`,  top: '50%', left: '50%', transform: `translate(-50%,50%)`}}>
                     <button className="btn btn-success position-absolute top-0 start-0" onClick={() => setRender('')}>Back</button>
-                    <button className="btn btn-danger position-absolute top-0 end-0" onClick={AddTaskModal}>Add Task</button>
+                    <button className="btn btn-danger position-absolute top-0 end-0" onClick={() => setModal(true)}>Add Task</button>
                     <div className="row justify-content-center p-5" style={{}}>  
                         {tasks ? tasks.map((task, i) => (
                             <div key={i} className='row'>
@@ -29,20 +30,23 @@ export default function TasksPage({tasks, setRender}) {
                                     <p name={task.task_desc} >{task.task_desc}</p>
                                 </div>
                                 <div className='col-2'>
-                                    <p>Url here</p>
+                                    <p>{task.task_url}</p>
                                 </div>
                                 <div className='col-2'>
                                     <p>Urgent</p>
+                                    {/* <p>{<UrgentStatus/>}</p> */}
                                 </div>
                                 <div className='col-2'>
-                                    <p>by 23/4</p>
+                                    <p>Date</p>
+                                    {/* <p>{<Date/>}</p> */}
                                 </div>
                             </div>
                         )) : undefined }   
                     </div>
                 </div>            
             </div>
-            </section>
+        </section>
+        { modal ? <AddTaskModal open={modal} setModal={setModal}/> : undefined }
     </div>
   )
 }
@@ -73,3 +77,45 @@ function CheckCircle({task}) {
         </div>
     )
 }
+
+function UrgentStatus({task}) {
+    const [activeComponent, setActiveComponent] = useState('')
+    useEffect(() => {
+
+        // if date is within 3 days -> urgent, 3-7 days -> important, 7+ days -> not important
+        switch (task.task_deadline) {
+            case 'Not Started':
+                setActiveComponent('Urgent')
+                break;
+            case 'Started':
+                setActiveComponent('Important')
+                break;
+            default:
+                setActiveComponent('Not Important')
+                break;
+        }
+    }, [task])
+
+    return (
+        <div>
+            {activeComponent}
+        </div>
+    )
+}
+
+/* 
+function Date({task}) {
+    const [activeComponent, setActiveComponent] = useState('')
+    useEffect(() => {
+
+        // change date format to 'by DD/MM'
+        
+    }, [task])
+
+    return (
+        <div>
+            {activeComponent}
+        </div>
+    )
+}
+*/
