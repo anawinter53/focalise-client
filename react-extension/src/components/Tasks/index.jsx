2a468b7e4a03e0054326bd5d4a5d041340f5df52
woejaddicor <your@emailadd.com>
2023-05-10 15:18:24 +0100

    Added styling for logged in buttons page

--- react-extension/src/components/Tasks/index.jsx
+++ react-extension/src/components/Tasks/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { User } from "../../contexts/user";
 import "./tasks.css";
 
@@ -5,13 +6,20 @@ export default function Tasks() {
   const { id } = User();
   // const id = localStorage.id
 
-  const getTasks = async () => {
-    const res = await fetch(`http://localhost:4000/tasks/user/${id}`);
-    const data = await res.json();
-    console.log(data);
-  };
+ useEffect(() => {
+   const getTasks = async () => {
+     const res = await fetch(`http://localhost:4000/tasks/user/${id}`);
+     const data = await res.json();
+     console.log(data)
+   };
+ getTasks() 
+ }, [])
   
   return (
-    <div>HELLO - Nothing to see here</div>
+    <ul>
+      <li></li>
+      <li></li>
+      <li></li>
+    </ul>
   )
 }
