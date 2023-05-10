3a72ae4060f1c4deb439a6e745c3414cccd435e8
Rosie <rosie@greyhoundcs.com>
2023-05-10 12:26:37 +0100

    resolved merge conflists with dev

--- src/pages/NotificationPage/index.jsx
+++ src/pages/NotificationPage/index.jsx
@@ -9,11 +9,6 @@ function NotificationPage() {
     const [countDown, setCountDown] = useState(null)
     const [breakCountDown, setBreakCountDown] = useState(null)
 
-    function buttonOnClick() {
-        setCountDown(10)
-
-    }
-
     useEffect(() => {
         let interval
         if (countDown > 0) {
@@ -47,6 +42,24 @@ function NotificationPage() {
         })
     }
 
+    useEffect(() => {
+        let interval
+        if (countDown > 0) {
+            interval = setInterval(() => {
+                setBreakCountDown(breakCountDown - 1)
+            }, 1000)
+        }
+        if (countDown === 0) {
+            sendBreakFinishedNotification()
+            setBreakCountDown(null)
+        }
+    
+        return () => {
+          clearInterval(interval);
+        };
+      }, [breakCountDown]);
+
+
     const sendBreakFinishedNotification = () => {
         addNotification({
             title: 'Break\'s Over!',
@@ -55,12 +68,14 @@ function NotificationPage() {
         })
     }
 
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        console.log(e.target.input)
+    }
+
 
     return <div className='notification'>
         <h1>Workplans page</h1>
-        <button onClick={buttonOnClick}>
-            10 second timer
-        </button>
         <p>Countdown: {countDown}</p>
         <div>
             {countDown ? <h2>Time Remaining: {Math.floor(countDown / 3600)} : {Math.floor((countDown % 3600)/60)} : {countDown % 60}</h2> : ""}
@@ -70,15 +85,21 @@ function NotificationPage() {
             <div className='get-started'>
                 <h2>Get started on a task</h2>
                 <p>Getting started is one of the hardest things. Set a timer for just to ten minutes and get one going.</p>
-                <button onClick={() => setCountDown(300)}>Start</button>
+                <button onClick={() => setCountDown(600)}>Start</button>
             </div>
             <div className='work-session'>
                 <h2>Session of work</h2>
                 <p>Set a timer for an hour, with regular breaks in the middle.</p>
+                <button onClick={() => setCountDown(3600)}>Start</button>
             </div>
             <div className="deadline">
                 <h2>Working to a deadline</h2>
                 <p>Set a time to finish a task by.</p>
+                <form onSubmit={handleSubmit}>
+                    <label htmlFor='deadline'>Set end time:</label>
+                    <input type='time' id='deadline'/>
+                    <button type='submit'>Start</button>
+                </form>
             </div>
         </div>
     </div>
